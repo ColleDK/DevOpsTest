c7e336d7aebd12d1af40ef557da40318e299e81d
= <wimo@boozt.com>
2022-08-30 11:31:16 +0200

    Added react project

---
+++ web/src/App.test.js
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn DevOps/i);
+  expect(linkElement).toBeInTheDocument();
+
+  const button = screen.getByRole("button");
+  expect(button).toHaveTextContent("Login");
+});
+
+test('login button on screen', () => {
+  render(<App />)
+  const button = screen.getByText("Login");
+  expect(button).toBeInTheDocument();
+})
