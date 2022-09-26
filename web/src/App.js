ea05e6c101207c36cee28462f3340e60b93ddaa6
= <wimo@boozt.com>
2022-09-26 10:05:29 +0200

    Added datastore

--- web/src/App.js
+++ web/src/App.js
@@ -1,15 +1,32 @@
 import './App.css';
 import {useState} from "react";
+import {Button, Grid, TextField} from "@mui/material";
+import {Link, Route, Routes, useParams, useNavigate} from "react-router-dom";
+import {giraffeStore} from "./stores/GiraffeStore";
+import {observer} from "mobx-react-lite";
 
 function App() {
+    let navigate = useNavigate()
     const [username, setUsername] = useState("Brian")
     return (
-        <div className="App">
-            <Greeting username={username}></Greeting>
-            <button onClick={ (e) => { setUsername("Johnnay") } }>
-                Skift navn
-            </button>
-        </div>
+        <Routes>
+            <Route exact path={"/"} element={
+                <div className="App">
+                    <ul>
+                        {giraffeStore.giraffes.map((giraffeName, key) =>
+                            <li key={key}>{giraffeName}</li>
+                        )}
+                    </ul>
+                    <Button onClick={() => giraffeStore.addGiraffe("Elmer")}>Tilføj giraf</Button>
+
+                    <Link to={"about/link"}>Go to about link</Link>
+                    <Button onClick={(e) => navigate("about/button")}>Go to about button</Button>
+                </div>
+            }>
+            </Route>
+            <Route path={"/about/:text"} element={<About></About>}></Route>
+            <Route path={"*"} element={<h1>Error 404</h1>}></Route>
+        </Routes>
     );
 }
 
@@ -17,4 +34,9 @@ const Greeting = ({username}) => {
     return (<h1>Hello, {username}</h1>)
 }
 
-export default App;
+const About = () => {
+    let {text} = useParams()
+    return <div><h1>About {text}</h1></div>
+}
+
+export default observer(App);
